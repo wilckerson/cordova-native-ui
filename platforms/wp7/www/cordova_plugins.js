a6ec73100b5962cf1c92a1d9cb49c91152136006
wilckersonganda <wilckerson@pinmypet.co>
2014-11-04 00:50:51 -0200

    wp7 platform

---
+++ platforms/wp7/www/cordova_plugins.js
@@ -0,0 +1,17 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/com.wilckerson.native_ui/www/native_ui.js",
+        "id": "com.wilckerson.native_ui.NativeUI",
+        "clobbers": [
+            "NativeUI"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "com.wilckerson.native_ui": "0.0.1"
+}
+// BOTTOM OF METADATA
+});
